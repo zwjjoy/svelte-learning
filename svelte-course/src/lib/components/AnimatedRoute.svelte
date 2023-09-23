044651d2f282e35da5958a596c1a0bba98557397
zwjjoy <zwjjoy@hotmail.com>
2023-09-23 14:21:45 +0000

    rename project to `svelte-course`

---
+++ svelte-course/src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from "$app/stores";
+    import { fly } from "svelte/transition";
+</script>
+
+{#key $page.url}
+    <div in:fly={{ x: '-100%', duration: 500}}>
+        <slot />
+    </div>
+{/key}
---
+++ svelte-course/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-green-300">Welcome to SvelteKit</h1>
+<p class="text-blue-300">This is a page inside a folder with a plus sign in its name.</p>
+
+<button class="btn">Hello daisyui</button>
