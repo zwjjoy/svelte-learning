ea590b212efb7cd2abd8017557a92f56ebe50d1f
zwjjoy <zwjjoy@hotmail.com>
2023-09-23 12:49:46 +0000

    lets build

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,10 +1,2 @@
-<script lang="ts">
-    import { onMount } from "svelte";
-
-    let todo = null;
-    
-    onMount(async () => {
-        todo = await fetch(".../todos").then(res => res.json());
-    });
-
-</script>
+<h1 class="text-green-300">Welcome to SvelteKit</h1>
+<p class="text-blue-300">This is a page inside a folder with a plus sign in its name.</p>
