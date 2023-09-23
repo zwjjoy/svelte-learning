126b56c9607f2d1d6f5bf05211c488cf96659115
zwjjoy <zwjjoy@hotmail.com>
2023-09-23 15:44:10 +0000

    custom usernames

---
+++ svelte-course/src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { user } from "$lib/firebase";
+</script>
+
+{#if $user}
+    <slot />
+{:else}
+    <p class="text-error text-center">
+        You must be signed in to view this page.
+        <a href="/login" class="btn btn-primary">Sign in</a>
+    </p>        
+{/if}
--- svelte-course/src/lib/firebase.ts
+++ svelte-course/src/lib/firebase.ts
@@ -5,20 +5,21 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from 'svelte/store';
 
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArRTjbUKOIgHAYXc2jUIkTjuGgk1urMfY",
-  authDomain: "quickstart-1561521230800.firebaseapp.com",
-  projectId: "quickstart-1561521230800",
-  storageBucket: "quickstart-1561521230800.appspot.com",
-  messagingSenderId: "514464852570",
-  appId: "1:514464852570:web:50ca15a0fc7f9865780663",
-  measurementId: "G-DMYX5RS7QL"
+	apiKey: "AIzaSyArRTjbUKOIgHAYXc2jUIkTjuGgk1urMfY",
+	authDomain: "quickstart-1561521230800.firebaseapp.com",
+	projectId: "quickstart-1561521230800",
+	storageBucket: "quickstart-1561521230800.appspot.com",
+	messagingSenderId: "514464852570",
+	appId: "1:514464852570:web:50ca15a0fc7f9865780663",
+	measurementId: "G-DMYX5RS7QL"
 };
 
 // Initialize Firebase
@@ -27,3 +28,35 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+
+/**
+ * @returns a store with the current firebase user
+ */
+
+function userStore() {
+
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser');
+		const { subscribe } = writable<User | null>(null);
+		return {
+		  subscribe,
+		}
+	  }
+	
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+	unsubscribe = onAuthStateChanged(auth, (user) => {
+		set(user);
+	});
+
+	return () => unsubscribe();
+	});
+
+	return {
+		subscribe,
+	};
+}
+	
+export const user = userStore();
